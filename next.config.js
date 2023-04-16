0aa97b35d7711834da7b5b3dd6b84cb691d57ff2
Luis Fernando <guffenix@gmail.com>
2023-04-16 11:18:42 -0500

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
+  output: 'export',
   images: { unoptimized: true },
   reactStrictMode: true,
   experimental: {
