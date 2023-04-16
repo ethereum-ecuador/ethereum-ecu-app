e6ed4f746819c5a608888b48d52e016d192ca4e6
Luis Fernando <guffenix@gmail.com>
2023-04-16 01:42:25 -0500

    build: update tailwind config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,14 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { NextUIProvider } from '@nextui-org/react';
-import type { AppProps } from 'next/app';
-import '@/styles/tailwind.css';
+import { useRouter } from 'next/router'
+import { NextUIProvider } from '@nextui-org/react'
+import type { AppProps } from 'next/app'
+import '@/styles/tailwind.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
+  const router = useRouter()
 
   return (
     <NextUIProvider>
       <Component {...pageProps} />
     </NextUIProvider>
-  );
+  )
 }
-
-
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,24 @@
 import Head from 'next/head'
 
 import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
 import { Newsletter } from '@/components/Newsletter'
 import { Schedule } from '@/components/Schedule'
-import { Speakers } from '@/components/Speakers'
 import { Sponsors } from '@/components/Sponsors'
-import  Nav  from '@/components/Navbar' 
-import {AboutComponent} from '@/components/About'
+import Nav from '@/components/Navbar'
+import { AboutComponent } from '@/components/About'
 import { Hero } from '@/components/Hero'
-
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>DeceptiConf - A community-driven design conference</title>
-        
-        </Head>
-        <Nav/>
-        {/* <Header /> */}
-        <main>
+      </Head>
+      <Nav />
+      <main>
         <Hero />
-        {/* <Speakers/> */}
         <Schedule />
-        <AboutComponent/>
+        <AboutComponent />
         <Sponsors />
         <Newsletter />
       </main>
