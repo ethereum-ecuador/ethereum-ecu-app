59759be19f3cf6a374aa4e38e10739f2520b8cf0
Luis Fernando <guffenix@gmail.com>
2023-04-17 19:34:10 -0500

    fix: use key prop correctly

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -1,24 +1,31 @@
 import { LogoWithText } from '@components/common/logo/index'
-import {footerLinks as links} from './footer.lib'
+import { footerLinks as links } from './footer.lib'
+import { nanoid } from 'nanoid'
 
 export function Footer() {
   return (
-    <footer className="overflow-hidden p-8 bg-white">
-      <div className="flex flex-col md:flex-row justify-center items-center md:justify-between gap-4">
-        <span className=''>
+    <footer className="overflow-hidden bg-white p-8">
+      <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:justify-between">
+        <span className="">
           <LogoWithText />
         </span>
-        <ul className="w-3/5 flex flex-wrap gap-2 sm:gap-6 justify-center md:justify-end items-center text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+        <ul className="flex w-3/5 flex-wrap items-center justify-center gap-2 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mb-0 sm:gap-6 md:justify-end">
           {links.map((link) => (
-            <li key={`f-${link}`}>
-              <a href={link.href} className="hover:underline">{link.name}</a>
+            <li key={`f-${link.name}-${nanoid()}`}>
+              <a href={link.href} className="hover:underline">
+                {link.name}
+              </a>
             </li>
           ))}
         </ul>
       </div>
-      <hr className="my-5 sm:my-6 border-gray-200 dark:border-gray-700 lg:my-8" />
-      <p className="text-sm text-gray-500 text-center dark:text-gray-400">
-        ETH Tricolor © 2023 ETH mitad del mundo, S.A.S B.I.C.<a href="https://www.eth-tricolor.org/" className="hover:underline">ETH Ecuador</a>.
+      <hr className="my-5 border-gray-200 dark:border-gray-700 sm:my-6 lg:my-8" />
+      <p className="text-center text-sm text-gray-500 dark:text-gray-400">
+        ETH Tricolor © 2023 ETH mitad del mundo, S.A.S B.I.C.
+        <a href="https://www.eth-tricolor.org/" className="hover:underline">
+          ETH Ecuador
+        </a>
+        .
         <br />
         Todos los derechos reservados.
       </p>
--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,34 +1,37 @@
-import React from 'react';
+import React from 'react'
 
 export const Hero = () => {
   return (
     <>
-    <div
-      className="relative bg-center bg-cover min-h-screen flex flex-col items-center justify-center"
-      style={{
-        backgroundImage: `linear-gradient(rgba(128, 0, 128, 0.2), rgba(128, 0, 128, 0.2)), url('https://cdn.leonardo.ai/users/c52f45ae-576d-4f2f-ae5a-01d5f5ae5827/generations/eba622f1-a078-4519-9c98-c8e75036beb3/variations/Default_Esta_impresionante_imagen_captura_la_esencia_de_la_mit_0_eba622f1-a078-4519-9c98-c8e75036beb3_1.jpg')`,
-      }}>
-        <div className="text-center relative z-10"> {/* Aumentamos el valor de z-index */}
-          <h2 className="text-white text-3xl md:text-4xl lg:text-5xl font-bold mb-2">
+      <div
+        className="relative flex min-h-screen flex-col items-center justify-center bg-cover bg-center"
+        style={{
+          backgroundImage: `linear-gradient(rgba(128, 0, 128, 0.2), rgba(128, 0, 128, 0.2)), url('./images/bg-eth.jpg')`,
+        }}
+      >
+        <div className="relative z-10 text-center">
+          {' '}
+          {/* Aumentamos el valor de z-index */}
+          <h2 className="mb-2 text-3xl font-bold text-white md:text-4xl lg:text-5xl">
             Road To
           </h2>
-          <h1 className="text-white text-5xl md:text-6xl lg:text-7xl font-bold mb-4">
+          <h1 className="mb-4 text-5xl font-bold text-white md:text-6xl lg:text-7xl">
             Blockchain Week
           </h1>
-          <p className="text-white text-xl md:text-2xl lg:text-3xl mb-6">
+          <p className="mb-6 text-xl text-white md:text-2xl lg:text-3xl">
             Quito, Ecuador
           </p>
-          <div className='flex flex-col justify-center items-center gap-4 md:flex-row'>
+          <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
             <a
               href="eventbrite.com"
-              target='_blank'
-              className="inline-block px-6 py-3 text-xl font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300"
+              target="_blank"
+              className="inline-block rounded-lg bg-blue-500 px-6 py-3 text-xl font-semibold text-white shadow-md transition-colors duration-300 hover:bg-blue-700"
             >
               Obtén tus tickets
             </a>
             <a
               href="#about"
-              className="inline-block px-6 py-3 text-xl font-semibold text-blue-500 bg-white border border-blue-500 rounded-lg shadow-md hover:bg-blue-100 transition-colors duration-300"
+              className="inline-block rounded-lg border border-blue-500 bg-white px-6 py-3 text-xl font-semibold text-blue-500 shadow-md transition-colors duration-300 hover:bg-blue-100"
             >
               Más información
             </a>
@@ -36,5 +39,5 @@ export const Hero = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
