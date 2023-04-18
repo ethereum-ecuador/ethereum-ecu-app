3efe25de57e1453d5d39bf714b302318a8d811b5
Alex Padilla <alex.padilla@epn.edu.ec>
2023-04-17 23:18:07 -0500

    feat(sponsors): update allies data

--- src/components/sponsors/sponsors.lib.ts
+++ src/components/sponsors/sponsors.lib.ts
@@ -4,11 +4,18 @@
 // import logoETH from '/images/eth-ec.png';
 
 import { SponsorProps } from "./sponsors.type";
-
+//hosts
 const logoUSFQ = '/images/hosts/usfq.svg';
 const logoEPN = '/images/hosts/epn.svg';
 const logoUDLA = '/images/hosts/udla.svg';
-const logoETH = '/images/eth-ec.svg';
+
+//allies
+const logoEthEC = '/images/allies/eth-ec.svg';
+const logoEF = '/images/allies/ef.svg';
+const logoImagenSistemas = '/images/allies/imagen-sistemas.jpg';
+const logoNovaEPN = '/images/allies/innovation-epn.png';
+const logoNovaUSFQ = '/images/allies/innovation-usfq.png';
+const logoLegalLab = '/images/allies/legal-lab.svg';
 
 interface SponsorList {
   [key: string]: SponsorProps[];
@@ -45,7 +52,7 @@ export const sponsors: SponsorList = {
   sponsors: [
     {
       name: 'Universidad de las Américas', logo: {
-        url: logoETH,
+        url: logoEthEC,
         alt: 'Logo UDLA',
         width: 125,
         height: 125
@@ -54,9 +61,41 @@ export const sponsors: SponsorList = {
   ],
   allies: [
     {
-      name: 'Universidad de las Américas', logo: {
-        url: logoETH,
+      name: 'Ethereum Foundation', logo: {
+        url: logoEF,
         alt: 'Logo UDLA',
+        width: 125,
+        height: 125
+      }
+    },
+    {
+      name: 'LegalLab USFQ', logo: {
+        url: logoLegalLab,
+        alt: 'Logo LegalLab USFQ',
+        width: 125,
+        height: 125
+      }
+    },
+    {
+      name: 'Imagen Sistemas EPN', logo: {
+        url: logoImagenSistemas,
+        alt: 'Logo Imagen Sistemas EPN',
+        width: 125,
+        height: 125
+      }
+    },
+    {
+      name: 'Club de Innovación USFQ', logo: {
+        url: logoNovaUSFQ,
+        alt: 'Logo Club de Innovación USFQ',
+        width: 125,
+        height: 125
+      }
+    },
+    {
+      name: 'Club de Innovación EPN', logo: {
+        url: logoNovaEPN,
+        alt: 'Club de Innovación EPN',
         width: 125,
         height: 125
       }
