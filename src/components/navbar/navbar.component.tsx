920094415d6aa09c7166966520ac99621eb7c324
Luis Fernando <guffenix@gmail.com>
2023-04-17 21:06:23 -0500

    feat: enable background in navbar

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,35 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { DesktopNavbar } from './desktop-navbar.component';
-import { MobileNavbar } from './mobile-navbar.component';
-import { links } from './navbar.lib';
+import React, { useState, useEffect } from 'react'
+import { DesktopNavbar } from './desktop-navbar.component'
+import { MobileNavbar } from './mobile-navbar.component'
+import { links } from './navbar.lib'
+import clsx from 'clsx'
 
-
-export const Navbar = () => {
-  const [scroll, setScroll] = useState(0);
-  const [background, setBackground] = useState('transparent');
+export const Navbar = ({ hasBackground }: NavbarProps) => {
+  const [_, setScroll] = useState(0)
+  const [background, setBackground] = useState(
+    hasBackground ? 'bg-[#241132]' : 'transparent'
+  )
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [background]);
+      window.removeEventListener('scroll', handleScroll)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [background])
 
   const handleScroll = () => {
-    const newScroll = window.pageYOffset;
-    setScroll(newScroll);
+    const newScroll = window.scrollY
+    setScroll(newScroll)
     if (newScroll > 50) {
-      setBackground('bg-[#241132]');
+      setBackground('bg-[#241132]')
     } else {
-      setBackground('bg-transparent');
+      if (!hasBackground) setBackground('bg-transparent')
     }
-  };
+  }
 
   return (
-    <header className={`w-full z-40 top-0 fixed transition-colors duration-500 ease-in-out ${background}`}>
-      <nav className='w-full text-white py-4'>
+    <header
+      className={clsx(
+        `fixed top-0 z-40 w-full transition-colors duration-500 ease-in-out ${background}`
+      )}
+    >
+      <nav className="w-full py-4 text-white">
         <span className="block md:hidden">
-            <MobileNavbar links={links} />
+          <MobileNavbar links={links} />
         </span>
         <span className="hidden md:block">
           <div className="flex flex-row items-center justify-between px-6">
@@ -38,11 +45,14 @@ export const Navbar = () => {
         </span>
       </nav>
     </header>
-  );
-};
-
+  )
+}
 
 export interface LinkProps {
-  name: string;
-  href: string;
+  name: string
+  href: string
+}
+
+interface NavbarProps {
+  hasBackground?: boolean
 }
