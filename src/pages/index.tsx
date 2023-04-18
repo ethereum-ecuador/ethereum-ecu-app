1cc7f551eef35fbd1b93f72215f2371ab6c9df35
Luis Fernando <guffenix@gmail.com>
2023-04-17 21:05:14 -0500

    chore: cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import Head from 'next/head'
 import { Footer } from '@/components/footer'
 import { Navbar } from '@/components/navbar'
 import { Hero } from '@/components/hero'
-import { Newsletter } from '@/components/Newsletter'
-import { Schedule } from '@/components/schedule'
 import { Sponsors } from '@/components/sponsors'
 import { Speakers } from '@/components/speakers'
 import About from '@/components/about/about.component'
@@ -14,19 +12,14 @@ export default function Home() {
     <>
       <Head>
         <title>Road to Blockchain Week</title>
-        <meta
-          name="description"
-          content="Road to Blockchain Week"
-        />
+        <meta name="description" content="Road to Blockchain Week" />
       </Head>
       <Navbar />
       <main>
         <Hero />
         <About />
         <Speakers />
-        <Schedule />
         <Sponsors />
-        {/* <Newsletter /> */}
       </main>
       <Footer />
     </>
