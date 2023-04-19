3264cf48ccf22500a4b61934989c57684ce5cfce
Alex Padilla <alex.padilla@epn.edu.ec>
2023-04-19 00:27:46 -0500

    feat: update speakers info

--- src/components/speakers/speakers.lib.ts
+++ src/components/speakers/speakers.lib.ts
@@ -73,6 +73,13 @@ export const speakerById: { [key: string]: Speaker } = {
         image: "/images/speakers/miguel-villegas.jpg",
         linkedin: "https://media.licdn.com/dms/image/D4E35AQFgFGSE1QQzIw/profile-framedphoto-shrink_800_800/0/1680206038456?e=1682395200&v=beta&t=aKTquGRdd7IaVJCP6nvdRcEO6x53Vc8-laWA5daR6jI",
     },
+    PaulNoboa: {
+        id: "PaulNoboa",
+        name: "Paul Noboa",
+        role: "",
+        image: "/images/speakers/paul-noboa.jpg",
+        linkedin: "https://www.linkedin.com/in/paulnoboavelasco/",
+    },
     JoseSebastianPonce: {
         id: "JoseSebastianPonce",
         name: "Jose Sebastian Ponce",
