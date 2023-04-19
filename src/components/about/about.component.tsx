e73816e7e0f315efde1b03d2aea26e46eefe4ab9
Alex Padilla <alex.padilla@epn.edu.ec>
2023-04-19 00:24:31 -0500

    refactor: comment video frame component

--- src/components/about/about.component.tsx
+++ src/components/about/about.component.tsx
@@ -14,9 +14,9 @@ export default function About() {
           <p className="text-justify font-display text-base md:text-lg lg:text-2xl font-normal tracking-tight text-blue-900 mt-4 md:mt-10">
             Una semana de conferencias sobre la tecnología blockchain que se llevará a cabo del 15 al 19 de mayo en varias universidades de Quito, como la EPN, USFQ y UDLA. Cada día tendrá un enfoque diferente relacionado con la implementación de blockchain en Ecuador, con varias conferencias sobre descentralización, seguridad y eficiencia. El evento tiene como objetivo promover y difundir la tecnología blockchain en el país.
           </p>
-          <div className=' my-6 w-[80vw] h-[30vh] sm:w-[70vw] sm:h-[50vh] lg:h-[50vh] max-w-[500px] max-h-[500px] xl:max-w-[750px] xl:max-h-[750px]' >
+          {/* <div className=' my-6 w-[80vw] h-[30vh] sm:w-[70vw] sm:h-[50vh] lg:h-[50vh] max-w-[500px] max-h-[500px] xl:max-w-[750px] xl:max-h-[750px]' >
             <iframe className='w-full h-full aspect-square' src="https://www.youtube.com/embed/ZrBYKYlffYI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-          </div>
+          </div> */}
         </div>
       </Container>
     </section>
