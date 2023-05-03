c5e1eceb5fcb51a6a4261b175fdf024499ab4e5a
Luis Fernando <guffenix@gmail.com>
2023-05-03 10:26:18 -0500

    chore: update developer information

--- src/components/contributors/contributors.lib.ts
+++ src/components/contributors/contributors.lib.ts
@@ -8,7 +8,7 @@ export const contributors: ContributorCardProps[] = [
     image: '/images/speakers/miguel-villegas.jpg',
     linkedin:
       'https://media.licdn.com/dms/image/D4E35AQFgFGSE1QQzIw/profile-framedphoto-shrink_800_800/0/1680206038456?e=1682395200&v=beta&t=aKTquGRdd7IaVJCP6nvdRcEO6x53Vc8-laWA5daR6jI',
-    github: 'https://github.com/NicoSerranoP',
+    github: 'https://github.com/mvpecu',
   },
   {
     id: 'LuisUshiña',
